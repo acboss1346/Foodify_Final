903a7f732aacbdfe94b7805c8ef5c3602637c8b5
Akshat Chauhan <akshatchauhan1346@gmail.com>
2025-11-22 22:59:10 +0530

    order

--- client/src/pages/Orders.jsx
+++ client/src/pages/Orders.jsx
@@ -1,1 +1,29 @@
 
+
+
+
+
+import { useEffect, useState } from "react";
+import { API } from "../api";
+
+export default function Orders() {
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    API.get("/orders/user").then((res) => setOrders(res.data));
+  }, []);
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>Your Orders</h2>
+
+      {orders.map((o) => (
+        <div className="menu-card" key={o.id}>
+          <p><b>Order ID:</b> {o.id}</p>
+          <p><b>Status:</b> {o.status}</p>
+          <p><b>Total:</b> ₹{o.total}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
